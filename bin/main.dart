6320df5075bc5b4f61301c99bfc7da91d054d682
Saklain Mustak <saklainmustak@sakmac.local>
2022-08-24 01:56:43 +0600

    main.dart smallest number

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,28 @@
 // findout total sum of this given array??
+
+// find out the largest number
+// media
+
+// find out sammelst number
 void main() {
   int sum = 0;
 
-  List<int> array = [23, 45, 555, 556, 56, 65];
-
-  sumArray(array, sum);
+  List<int> array = [23, 45, 555, 556, 56, 65, 18];
+  int min = array[0];
+  sumArray(array, sum, min);
 }
 
-int sumArray(array, sum) {
+int sumArray(array, sum, min) {
   for (int i = 0; i < array.length; i++) {
-    sum = sum + array[i];
+    // sum = sum + array[i];
+    if (array[i] < min) {
+      min = array[i];
+    }
+    print("value: ${array[i]}");
+    print('min value : ${min}');
   }
-  print('total of array is ${sum}');
-  return sum;
+  //print('total of array is ${sum}');
+  //return sum;
+  print(" Smallest number ${min}");
+  return min;
 }
